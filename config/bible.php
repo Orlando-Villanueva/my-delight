51df658e98201b622eea18ec20bea8f7a5f7fc65
Orlando <orlandovillanueva11@gmail.com>
2025-06-18 22:17:55 -0400

    feat: add Bible configuration and language files for English and French, including book details and testament structure

---
+++ config/bible.php
@@ -0,0 +1,351 @@
+<?php
+
+return [
+    'books' => [
+        // Old Testament (1-39)
+        1 => [
+            'id' => 1,
+            'chapters' => 50,
+            'testament' => 'old'
+        ],
+        2 => [
+            'id' => 2,
+            'chapters' => 40,
+            'testament' => 'old'
+        ],
+        3 => [
+            'id' => 3,
+            'chapters' => 27,
+            'testament' => 'old'
+        ],
+        4 => [
+            'id' => 4,
+            'chapters' => 36,
+            'testament' => 'old'
+        ],
+        5 => [
+            'id' => 5,
+            'chapters' => 34,
+            'testament' => 'old'
+        ],
+        6 => [
+            'id' => 6,
+            'chapters' => 24,
+            'testament' => 'old'
+        ],
+        7 => [
+            'id' => 7,
+            'chapters' => 21,
+            'testament' => 'old'
+        ],
+        8 => [
+            'id' => 8,
+            'chapters' => 4,
+            'testament' => 'old'
+        ],
+        9 => [
+            'id' => 9,
+            'chapters' => 31,
+            'testament' => 'old'
+        ],
+        10 => [
+            'id' => 10,
+            'chapters' => 24,
+            'testament' => 'old'
+        ],
+        11 => [
+            'id' => 11,
+            'chapters' => 22,
+            'testament' => 'old'
+        ],
+        12 => [
+            'id' => 12,
+            'chapters' => 25,
+            'testament' => 'old'
+        ],
+        13 => [
+            'id' => 13,
+            'chapters' => 29,
+            'testament' => 'old'
+        ],
+        14 => [
+            'id' => 14,
+            'chapters' => 36,
+            'testament' => 'old'
+        ],
+        15 => [
+            'id' => 15,
+            'chapters' => 10,
+            'testament' => 'old'
+        ],
+        16 => [
+            'id' => 16,
+            'chapters' => 13,
+            'testament' => 'old'
+        ],
+        17 => [
+            'id' => 17,
+            'chapters' => 10,
+            'testament' => 'old'
+        ],
+        18 => [
+            'id' => 18,
+            'chapters' => 42,
+            'testament' => 'old'
+        ],
+        19 => [
+            'id' => 19,
+            'chapters' => 150,
+            'testament' => 'old'
+        ],
+        20 => [
+            'id' => 20,
+            'chapters' => 31,
+            'testament' => 'old'
+        ],
+        21 => [
+            'id' => 21,
+            'chapters' => 12,
+            'testament' => 'old'
+        ],
+        22 => [
+            'id' => 22,
+            'chapters' => 8,
+            'testament' => 'old'
+        ],
+        23 => [
+            'id' => 23,
+            'chapters' => 66,
+            'testament' => 'old'
+        ],
+        24 => [
+            'id' => 24,
+            'chapters' => 52,
+            'testament' => 'old'
+        ],
+        25 => [
+            'id' => 25,
+            'chapters' => 5,
+            'testament' => 'old'
+        ],
+        26 => [
+            'id' => 26,
+            'chapters' => 48,
+            'testament' => 'old'
+        ],
+        27 => [
+            'id' => 27,
+            'chapters' => 12,
+            'testament' => 'old'
+        ],
+        28 => [
+            'id' => 28,
+            'chapters' => 14,
+            'testament' => 'old'
+        ],
+        29 => [
+            'id' => 29,
+            'chapters' => 3,
+            'testament' => 'old'
+        ],
+        30 => [
+            'id' => 30,
+            'chapters' => 9,
+            'testament' => 'old'
+        ],
+        31 => [
+            'id' => 31,
+            'chapters' => 1,
+            'testament' => 'old'
+        ],
+        32 => [
+            'id' => 32,
+            'chapters' => 4,
+            'testament' => 'old'
+        ],
+        33 => [
+            'id' => 33,
+            'chapters' => 7,
+            'testament' => 'old'
+        ],
+        34 => [
+            'id' => 34,
+            'chapters' => 3,
+            'testament' => 'old'
+        ],
+        35 => [
+            'id' => 35,
+            'chapters' => 3,
+            'testament' => 'old'
+        ],
+        36 => [
+            'id' => 36,
+            'chapters' => 3,
+            'testament' => 'old'
+        ],
+        37 => [
+            'id' => 37,
+            'chapters' => 2,
+            'testament' => 'old'
+        ],
+        38 => [
+            'id' => 38,
+            'chapters' => 14,
+            'testament' => 'old'
+        ],
+        39 => [
+            'id' => 39,
+            'chapters' => 4,
+            'testament' => 'old'
+        ],
+
+        // New Testament (40-66)
+        40 => [
+            'id' => 40,
+            'chapters' => 28,
+            'testament' => 'new'
+        ],
+        41 => [
+            'id' => 41,
+            'chapters' => 16,
+            'testament' => 'new'
+        ],
+        42 => [
+            'id' => 42,
+            'chapters' => 24,
+            'testament' => 'new'
+        ],
+        43 => [
+            'id' => 43,
+            'chapters' => 21,
+            'testament' => 'new'
+        ],
+        44 => [
+            'id' => 44,
+            'chapters' => 28,
+            'testament' => 'new'
+        ],
+        45 => [
+            'id' => 45,
+            'chapters' => 16,
+            'testament' => 'new'
+        ],
+        46 => [
+            'id' => 46,
+            'chapters' => 16,
+            'testament' => 'new'
+        ],
+        47 => [
+            'id' => 47,
+            'chapters' => 13,
+            'testament' => 'new'
+        ],
+        48 => [
+            'id' => 48,
+            'chapters' => 6,
+            'testament' => 'new'
+        ],
+        49 => [
+            'id' => 49,
+            'chapters' => 6,
+            'testament' => 'new'
+        ],
+        50 => [
+            'id' => 50,
+            'chapters' => 4,
+            'testament' => 'new'
+        ],
+        51 => [
+            'id' => 51,
+            'chapters' => 4,
+            'testament' => 'new'
+        ],
+        52 => [
+            'id' => 52,
+            'chapters' => 5,
+            'testament' => 'new'
+        ],
+        53 => [
+            'id' => 53,
+            'chapters' => 3,
+            'testament' => 'new'
+        ],
+        54 => [
+            'id' => 54,
+            'chapters' => 6,
+            'testament' => 'new'
+        ],
+        55 => [
+            'id' => 55,
+            'chapters' => 4,
+            'testament' => 'new'
+        ],
+        56 => [
+            'id' => 56,
+            'chapters' => 3,
+            'testament' => 'new'
+        ],
+        57 => [
+            'id' => 57,
+            'chapters' => 1,
+            'testament' => 'new'
+        ],
+        58 => [
+            'id' => 58,
+            'chapters' => 13,
+            'testament' => 'new'
+        ],
+        59 => [
+            'id' => 59,
+            'chapters' => 5,
+            'testament' => 'new'
+        ],
+        60 => [
+            'id' => 60,
+            'chapters' => 5,
+            'testament' => 'new'
+        ],
+        61 => [
+            'id' => 61,
+            'chapters' => 3,
+            'testament' => 'new'
+        ],
+        62 => [
+            'id' => 62,
+            'chapters' => 5,
+            'testament' => 'new'
+        ],
+        63 => [
+            'id' => 63,
+            'chapters' => 1,
+            'testament' => 'new'
+        ],
+        64 => [
+            'id' => 64,
+            'chapters' => 1,
+            'testament' => 'new'
+        ],
+        65 => [
+            'id' => 65,
+            'chapters' => 1,
+            'testament' => 'new'
+        ],
+        66 => [
+            'id' => 66,
+            'chapters' => 22,
+            'testament' => 'new'
+        ],
+    ],
+
+    'testaments' => [
+        'old' => [
+            'range' => [1, 39]
+        ],
+        'new' => [
+            'range' => [40, 66]
+        ]
+    ],
+
+    'default_locale' => 'en',
+    'supported_locales' => ['en', 'fr']
+]; 
