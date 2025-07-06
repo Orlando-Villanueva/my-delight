f1b9680733e138e5527ef3ce9e5b03e00adab40e
Orlando <orlandovillanueva11@gmail.com>
2025-07-06 18:46:45 -0400

    Remove default seeder from TestCase to streamline test setup, enhancing clarity and reducing unnecessary complexity.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,11 +8,4 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 abstract class TestCase extends BaseTestCase
 {
     use DatabaseTransactions;
-    
-    /**
-     * Indicates whether the default seeder should run before each test.
-     *
-     * @var bool
-     */
-    protected $seed = true;
 }
