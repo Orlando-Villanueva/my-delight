86bd61a7e691d4cfeb65a30efc989995dafb9f87
Orlando <orlandovillanueva11@gmail.com>
2025-10-12 11:52:09 -0400

    refactor: update Log Reading button icon size and SVG path for improved clarity

--- resources/views/components/navigation/mobile-bottom-bar.blade.php
+++ resources/views/components/navigation/mobile-bottom-bar.blade.php
@@ -22,10 +22,8 @@
             <button type="button" hx-get="{{ route('logs.create') }}" hx-target="#page-container" hx-swap="innerHTML"
                 hx-push-url="true"
                 class="inline-flex items-center justify-center w-10 h-10 font-medium bg-accent-500 rounded-full hover:bg-accent-600 group focus:ring-4 focus:ring-accent-300 focus:outline-none dark:focus:ring-accent-800">
-                <svg class="w-4 h-4 text-white" aria-hidden="true" fill="none" stroke="currentColor"
-                    viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                <svg class="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
                 </svg>
                 <span class="sr-only">Log Reading</span>
             </button>
